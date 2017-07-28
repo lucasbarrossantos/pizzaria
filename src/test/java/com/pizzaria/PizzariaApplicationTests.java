a2ba0edde1002ce805b03fdf3ace65e9a13ae9a1
lucasbarrossantos <lucas-barros28@hotmail.com>
2017-07-28 18:29:46 -0300

    Pizzaria

---
+++ src/test/java/com/pizzaria/PizzariaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pizzaria;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PizzariaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
