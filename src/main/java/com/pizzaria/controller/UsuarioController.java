143d9281920345c82d23b03671beb4e8fcd84918
lucasbarrossantos <lucas-barros28@hotmail.com>
2017-08-02 20:28:13 -0300

    Pizzaria

--- src/main/java/com/pizzaria/controller/UsuarioController.java
+++ src/main/java/com/pizzaria/controller/UsuarioController.java
@@ -1,9 +1,15 @@
 package com.pizzaria.controller;
 
+import com.pizzaria.controller.page.PageWrapper;
 import com.pizzaria.model.Usuario;
 import com.pizzaria.repository.Grupos;
+import com.pizzaria.repository.Usuarios;
 import com.pizzaria.service.UsuariosService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @Controller
@@ -25,6 +32,9 @@ public class UsuarioController {
 
     @Autowired
     private UsuariosService usuariosService;
+
+    @Autowired
+    private Usuarios usuarios;
 
     @GetMapping("/new")
     public ModelAndView novo(Usuario usuario){
@@ -43,6 +53,26 @@ public class UsuarioController {
         usuariosService.salvar(usuario);
         attributes.addFlashAttribute("mensagem", "Usuário salvo com sucesso!");
         return new ModelAndView("redirect:/usuarios/new");
+    }
+
+    @GetMapping
+    public ModelAndView pesquisar(Usuario usuario, Pageable pageable,
+                                  HttpServletRequest httpServletRequest){
+
+        ModelAndView mv = new ModelAndView("usuario/PesquisarUsuario");
+
+        ExampleMatcher matcher = ExampleMatcher.matching()
+                .withMatcher("nome", where -> where.startsWith().ignoreCase())
+                .withMatcher("cpf",  where -> where.startsWith().ignoreCase());
+
+        Page<Usuario> page = usuarios.findAll(Example.of(usuario, matcher), pageable);
+
+        PageWrapper<Usuario> paginaWrapper =
+                new PageWrapper<>(page, httpServletRequest);
+
+        mv.addObject("pagina", paginaWrapper);
+        return mv;
+
     }
 
 }
