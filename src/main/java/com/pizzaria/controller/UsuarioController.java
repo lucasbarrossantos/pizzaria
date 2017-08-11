45114648e0db02b2f61e67eb124c3a30d5f7584b
Lucas Barros <lucas-barros28@hotmail.com>
2017-08-11 16:48:32 -0300

    Pizzaria

--- src/main/java/com/pizzaria/controller/UsuarioController.java
+++ src/main/java/com/pizzaria/controller/UsuarioController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -73,6 +74,13 @@ public class UsuarioController {
         mv.addObject("pagina", paginaWrapper);
         return mv;
 
+    }
+
+    @GetMapping("/{id}")
+    public ModelAndView editar(@PathVariable("id") Usuario usuario){
+        ModelAndView mv = novo(usuario);
+        mv.addObject(usuario);
+        return mv;
     }
 
 }
