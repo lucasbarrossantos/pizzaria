6dfff0cb09ac8209012d56dc1e8ddf8d88fe0978
lucasbarrossantos <lucas-barros28@hotmail.com>
2017-08-14 13:44:29 -0300

    Pizzaria

--- src/main/java/com/pizzaria/model/Promocao.java
+++ src/main/java/com/pizzaria/model/Promocao.java
@@ -33,7 +33,7 @@ public class Promocao {
     private BigDecimal valor;
 
     @Size(min = 1, message = "Selecione pelo menos uma pizza")
-    @OneToMany(mappedBy = "promocao", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    @OneToMany(mappedBy = "promocao", fetch = FetchType.LAZY, cascade = CascadeType.MERGE)
     private List<Pizza> pizzas = new ArrayList<>();
 
     @ManyToOne
@@ -77,12 +77,6 @@ public class Promocao {
 
     public void setEstabelecimento(Estabelecimento estabelecimento) {
         this.estabelecimento = estabelecimento;
-    }
-
-    @PrePersist
-    @PreUpdate
-    private void prePersistPreUpdate(){
-        this.pizzas.forEach(pizza -> pizza.setPromocao(this));
     }
 
     @Override
--- src/main/java/com/pizzaria/service/PromocoesService.java
+++ src/main/java/com/pizzaria/service/PromocoesService.java
@@ -7,8 +7,11 @@ import com.pizzaria.repository.Promocoes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 public class PromocoesService {
@@ -22,14 +25,26 @@ public class PromocoesService {
     public Promocao salvar(Promocao promocao) {
         List<String> pizzasPromocao = pizzas.pizzasEmPromocao().stream()
                 .filter(pizza -> promocao.getPizzas().contains(pizza))
-                    .map(Pizza::getSaborPizza)
+                .map(Pizza::getSaborPizza)
                 .collect(Collectors.toList());
 
-        if (pizzasPromocao.size() > 0 && promocao.getId() != null) {
+        if (pizzasPromocao.size() > 0 && promocao.getId() == null) {
             throw new RuntimeException("Já existe promoção para: " + pizzasPromocao);
         }
 
+        removendoPizzaDaPromocao(promocao);
+        promocao.getPizzas().forEach(pizza -> pizza.setPromocao(promocao));
         return promocoes.save(promocao);
+    }
+
+    private void removendoPizzaDaPromocao(Promocao promocao) {
+        pizzas.pizzasEmPromocao()
+                .stream()
+                .filter(pizza -> pizza.getPromocao().getId().equals(promocao.getId()))
+                .forEach(pizza -> {
+                    pizza.setPromocao(null);
+                    pizzas.save(pizza);
+                });
     }
 
 }
