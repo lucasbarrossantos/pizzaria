4d60ad513dfee1ca344d08242246415230149665
lucasbarrossantos <lucas-barros28@hotmail.com>
2017-08-14 13:45:08 -0300

    Pizzaria

--- src/main/java/com/pizzaria/service/PromocoesService.java
+++ src/main/java/com/pizzaria/service/PromocoesService.java
@@ -6,12 +6,8 @@ import com.pizzaria.repository.Pizzas;
 import com.pizzaria.repository.Promocoes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Service
 public class PromocoesService {
