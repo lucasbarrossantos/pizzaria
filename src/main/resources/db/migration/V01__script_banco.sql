a14c52efdae166f98215cb6c3e77d5372553a9dc
Lucas Barros <lucas-barros28@hotmail.com>
2017-08-29 09:06:05 -0300

    Pizzaria

--- src/main/resources/db/migration/V01__script_banco.sql
+++ src/main/resources/db/migration/V01__script_banco.sql
@@ -105,23 +105,6 @@ CREATE TABLE grupo_permissao (
   FOREIGN KEY (codigo_permissao) REFERENCES permissao (id),
   FOREIGN KEY (codigo_grupo) REFERENCES grupo (id)
 ) ;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
-
-DROP TABLE IF EXISTS pedido;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE pedido (
-  id SERIAL NOT NULL ,
-  data_pedido date DEFAULT NULL,
-  observacao varchar(255) DEFAULT NULL,
-  status varchar(255) DEFAULT NULL,
-  valor_total decimal(19,2) DEFAULT NULL,
-  codigo_usuario INT DEFAULT NULL,
-  PRIMARY KEY (id),
-  FOREIGN KEY (codigo_usuario) REFERENCES usuario (id)
-) ;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 DROP TABLE IF EXISTS mesa;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
@@ -300,15 +283,20 @@ CREATE TABLE usuario (
   PRIMARY KEY (id),
   FOREIGN KEY (estabelecimento_id) REFERENCES estabelecimento (id)
 );
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table usuario
---
-
---
--- Table structure for table usuario_grupo
---
+DROP TABLE IF EXISTS pedido;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE pedido (
+  id SERIAL NOT NULL ,
+  data_pedido date DEFAULT NULL,
+  observacao varchar(255) DEFAULT NULL,
+  status varchar(255) DEFAULT NULL,
+  valor_total decimal(19,2) DEFAULT NULL,
+  codigo_usuario INT DEFAULT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (codigo_usuario) REFERENCES usuario (id)
+) ;
 
 DROP TABLE IF EXISTS usuario_grupo;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
