eac44ab133dd090cfaf21f116cad73d757894a0d
Lucas Barros <lucas-barros28@hotmail.com>
2017-08-29 14:22:40 -0300

    Pizzaria

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -16,15 +16,9 @@ INSERT INTO permissao VALUES (2, 'VENDEDOR');
 INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) VALUES (1, 1);
 INSERT INTO grupo_permissao (codigo_grupo, codigo_permissao) VALUES (2, 2);
 
-INSERT INTO usuario_grupo (codigo_usuario, codigo_grupo) VALUES (
-  (SELECT id
-   FROM usuario
-   WHERE email = 'admin@admin.com'), 1);
+INSERT INTO usuario_grupo (codigo_usuario, codigo_grupo) VALUES (1, 1);
 
-INSERT INTO usuario_grupo (codigo_usuario, codigo_grupo) VALUES (
-  (SELECT id
-   FROM usuario
-   WHERE email = 'vendas@vendas.com'), 2);
+INSERT INTO usuario_grupo (codigo_usuario, codigo_grupo) VALUES (2, 2);
 
 --- Inserir permissões
 
