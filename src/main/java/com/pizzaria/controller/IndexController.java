9cddc5af8b408473db670e772eadccb8a218e2c2
lucasbarrossantos <lucas-barros28@hotmail.com>
2017-09-04 19:48:44 -0300

    Pizzaria

--- src/main/java/com/pizzaria/controller/IndexController.java
+++ src/main/java/com/pizzaria/controller/IndexController.java
@@ -1,7 +1,17 @@
 package com.pizzaria.controller;
 
+import com.pizzaria.repository.Fornecedores;
+import com.pizzaria.repository.Mesas;
+import com.pizzaria.repository.Produtos;
+import com.pizzaria.repository.Titulos;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.math.BigDecimal;
+import java.time.MonthDay;
+import java.util.Optional;
 
 /**
  * Created by lucasbarros on 26/07/2017.
@@ -12,9 +22,26 @@ public class IndexController {
 
     private static final String VIEW = "index";
 
+    @Autowired
+    private Fornecedores fornecedores;
+
+    @Autowired
+    private Produtos produtos;
+
+    @Autowired
+    private Mesas mesas;
+
+    @Autowired
+    private Titulos titulos;
+
     @GetMapping("/")
-    public String index() {
-        return VIEW;
+    public ModelAndView index() {
+        ModelAndView mv = new ModelAndView(VIEW);
+        mv.addObject("fornecedores", fornecedores.findAll());
+        mv.addObject("produtos", produtos.findAll());
+        mv.addObject("mesas", mesas.findAll());
+        mv.addObject("vendasNoMes", titulos.valorTotalNoMes(MonthDay.now().getMonthValue()).orElse(BigDecimal.ZERO));
+        return mv;
     }
 
 }
--- src/main/java/com/pizzaria/repository/Titulos.java
+++ src/main/java/com/pizzaria/repository/Titulos.java
@@ -2,9 +2,16 @@ package com.pizzaria.repository;
 
 import com.pizzaria.model.Titulo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.util.Optional;
 
 @Repository
 public interface Titulos extends JpaRepository<Titulo, Long> {
+
+    @Query("select sum (t.valorPago) from Titulo as t where month(t.dataDeEmissao) = ?1")
+    Optional<BigDecimal> valorTotalNoMes(int monthValue);
 
 }
