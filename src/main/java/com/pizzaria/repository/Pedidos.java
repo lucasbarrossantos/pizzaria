741410c0e8b6d05d3e9127d4c827c45fb28d0d84
lucasbarrossantos <lucas-barros28@hotmail.com>
2017-09-10 11:51:57 -0300

    Gráfico do dashboard

--- src/main/java/com/pizzaria/repository/Pedidos.java
+++ src/main/java/com/pizzaria/repository/Pedidos.java
@@ -13,11 +13,11 @@ import java.util.List;
 @Repository
 public interface Pedidos extends JpaRepository<Pedido, Long> {
 
-    @Query("SELECT new com.pizzaria.service.dto.PedidoDTO(date_format(p.dataPedido, '%m/%Y') as mes, sum (p.valorTotal)) " +
+    @Query("SELECT new com.pizzaria.service.dto.PedidoDTO(to_char(p.data_pedido::date, 'MM/YY')  as mes,   sum(p.valor_total) as total) " +
             " FROM Pedido AS p " +
             " where p.status = 'CONCLUIDO' \n" +
             " and p.dataPedido BETWEEN ?1 AND ?2 \n " +
-            " group by date_format(p.dataPedido, '%m/%Y')\n" +
-            " order by date_format(p.dataPedido, '%m/%Y') DESC ")
+            " group by to_char(p.data_pedido::date, 'MM/YY') \n" +
+            " order by to_char(p.data_pedido::date, 'MM/YY') ASC ")
     List<PedidoDTO> totalPorMes(LocalDate seisMesesAtras, LocalDate hoje);
 }
