5d9d1a1854f64d2822f9ec507f43e496f2b1773a
lucasbarrossantos <lucas-barros28@hotmail.com>
2017-09-10 11:26:13 -0300

    Gráfico do dashboard

--- src/main/java/com/pizzaria/repository/Usuarios.java
+++ src/main/java/com/pizzaria/repository/Usuarios.java
@@ -3,11 +3,13 @@ package com.pizzaria.repository;
 import com.pizzaria.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface Usuarios extends JpaRepository<Usuario, Long> {
 
     Optional<Usuario> findByEmailIgnoreCase(String email);
